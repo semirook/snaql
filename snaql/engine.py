180d044d83becfc1ce0ea298a2b0f14b2057015c
David.Smit <david.smit@gkn.com>
2020-11-11 08:38:49 -0500

    Added a pandas and pyodbc engine

--- snaql/engine.py
+++ snaql/engine.py
@@ -1,4 +1,7 @@
-def default(query_string: str, connection: str):
+import pyodbc
+import pandas as pd
+
+def default(query_string: str, connection_string: str):
     """
     :param query_string: This is the raw query string that the SQL engine would use to return the data
     :param connection: This is the connection string that comes from the SQL template that can be used for the SQL query
@@ -6,3 +9,18 @@ def default(query_string: str, connection: str):
     would return data (ie DataFrame)
     """
     return query_string
+
+
+def pandas(query_string: str, connection_string: str):
+    """
+    :param query_string: The raw query string to execute
+    :param connection: Connection string formatted for use by PYODBC
+    :return: A pandas DataFrame
+    """
+    try:
+        connection = pyodbc.Connection(connection_string)
+        return pd.read_sql_query(query_string, connection)
+    except Exception as e:
+        print(e)
+    finally:
+        connection.close()
