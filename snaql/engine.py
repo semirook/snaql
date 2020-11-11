58814261caea1f511ba787125ea2946c79bd95c4
David.Smit <david.smit@gkn.com>
2020-11-11 07:36:53 -0500

    Added a default engine (only returns the query string)

---
+++ snaql/engine.py
@@ -0,0 +1,8 @@
+def default(query_string: str, connection: str):
+    """
+    :param query_string: This is the raw query string that the SQL engine would use to return the data
+    :param connection: This is the connection string that comes from the SQL template that can be used for the SQL query
+    :return: The default will return the string w/o changes to keep the default SNAQL behavior, but any additional
+    would return data (ie DataFrame)
+    """
+    return query_string
