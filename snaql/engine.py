6488eb9bc65cd6eda6eb424ba287dcc6e5538145
David.Smit <david.smit@gkn.com>
2020-11-14 23:09:42 -0500

    removed the pandas/pyodbc engine

--- snaql/engine.py
+++ snaql/engine.py
@@ -10,18 +10,3 @@ def default(query_string: str, connection_string: str):
     """
     return query_string
 
-
-def pandas(query_string: str, connection_string: str):
-    """
-    :param query_string: The raw query string to execute
-    :param connection: Connection string formatted for use by PYODBC
-    :return: A pandas DataFrame
-    """
-    connection = pyodbc.connect(connection_string)
-    try:
-        return pd.read_sql_query(query_string, connection)
-    except Exception as e:
-        print(e)
-    finally:
-        connection.close()
-
